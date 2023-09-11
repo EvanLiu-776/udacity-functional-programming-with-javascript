db1fae99e6e6f51aba9b4f4575f4666ae879e23d
Evan Liu <evanliunet@gmail.com>
2023-09-11 12:15:57 -0400

    first commit

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,52 +1,87 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
 
-// add our markup to the page
-const root = document.getElementById('root')
+const updateStore = function name() {
+    let store = Immutable.Map({
+        user: { name: "Udacity" },
+        apod: '',
+        rovers: { Curiosity: { name: "Curiosity" }, Opportunity: { name: 'Opportunity' }, Spirit: { name: 'Spirit' } },
+        selectedRover: 'Curiosity'
+    })
+    return function (newState) {
+        store = store.mergeDeep(newState)
+        const root = document.getElementById('root')
+        render(root, store)
+    }
+}()
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
+
+// create content
+// const App = (state) => {
+//     let { rovers, apod } = state
+
+
+//     return `
+//         <header></header>
+//         <main>
+//             ${Greeting(store.user.name)}
+//             <section>
+//                 <h3>Put things on the page!</h3>
+//                 <p>Here is an example section.</p>
+//                 <p>
+//                     One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
+//                     the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
+//                     This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
+//                     applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
+//                     explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
+//                     but generally help with discoverability of relevant imagery.
+//                 </p>
+//                 ${ImageOfTheDay(apod)}
+//             </section>
+//         </main>
+//         <footer></footer>
+//     `
+// }
+const App = (state) => {
+    const rovers = state.get("rovers")
+    const roverInStore = rovers[state.get("selectedRover")];
+    return `<header></header>
+        <main>
+            <section>
+                <h3>Rovers</h3>
+                <div class="tab_bar">
+                    ${renderRoverName(rovers, state)}
+                </div>
+
+                <div class="rover_detail_container">
+                    ${RoverContent(roverInStore)}
+                </div>
+            </section>
+        </main>
+        <footer></footer>`
 }
 
 const render = async (root, state) => {
+    // root.innerHTML = App(state)
     root.innerHTML = App(state)
 }
 
 
-// create content
-const App = (state) => {
-    let { rovers, apod } = state
+// function roverNameClickHandler(e) {
+//     store.selectedRover = e.target.textContent;
+//     const root = document.getElementById('root')
 
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
-}
+//     render(root, store);
+// }
+
+
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store)
+    updateStore({})
+    const allRoverNames = document.querySelectorAll(".rover_name");
+    // allRoverNames.forEach((roverNameEle) => roverNameEle.addEventListener("click", roverNameClickHandler))
 })
+
+
 
 // ------------------------------------------------------  COMPONENTS
 
@@ -63,7 +98,36 @@ const Greeting = (name) => {
     `
 }
 
+const switchTab = (roverName) => {
+    updateStore({ selectedRover: roverName })
+}
+
+const renderRoverName = (rovers, state) => {
+    const selectedRover = state.get("selectedRover")
+    return Object.values(rovers).reduce((prev, cur) => prev + `<span class="rover_name" onclick="switchTab('${cur.name}')"  id=${selectedRover === cur.name ? "selected_rover" : ""}>${cur.name}</span>`, "")
+}
+
 // Example of a pure function that renders infomation requested from the backend
+const RoverContent = (roverContent) => {
+    if (!roverContent.max_date) {
+        getRoverContent(roverContent.name);
+        return ``
+    } else {
+        if (!roverContent.photo) {
+            getRoverImage(roverContent)
+        }
+        return (`
+            <div class="rover_detail">
+                <div class="detail_row"><p>Name:</p> <p>${roverContent.name}</p></div>
+                <div class="detail_row"><p>Landing Date:</p> <p>${roverContent.landing_date}</p></div>
+                <div class="detail_row"><p>Launch Date:</p> <p>${roverContent.launch_date}</p></div>
+                <div class="detail_row"><p>Status:</p> <p>${roverContent.status}</p></div>
+                <div>Latest Picture:</div>
+                <img src="${roverContent.photo || ""}" alt="rover image loading" />
+            </div>
+                `)
+    }
+}
 const ImageOfTheDay = (apod) => {
 
     // If image does not already exist, or it is not from today -- request it again
@@ -72,7 +136,7 @@ const ImageOfTheDay = (apod) => {
     console.log(photodate.getDate(), today.getDate());
 
     console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
+    if (!apod || apod.date === today.getDate()) {
         getImageOfTheDay(store)
     }
 
@@ -94,12 +158,28 @@ const ImageOfTheDay = (apod) => {
 // ------------------------------------------------------  API CALLS
 
 // Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+// const getImageOfTheDay = (state) => {
+//     let { apod } = state
 
-    fetch(`http://localhost:3000/apod`)
+//     fetch(`http://localhost:3000/apod`)
+//         .then(res => res.json())
+//         .then(apod => updateStore(store, { apod }))
+
+//     return data
+// }
+
+
+
+const getRoverContent = (roverName) => {
+
+    fetch(`http://localhost:3000/rovers?rover_name=${roverName}`)
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+        .then(({ rover }) => updateStore({ rovers: { [rover.name]: rover } }))
+}
 
-    return data
+
+const getRoverImage = (rover) => {
+    fetch(`http://localhost:3000/roverimage?rover_name=${rover.name}&earth_date=${rover.max_date}`)
+        .then(res => res.json())
+        .then(({ photo }) => updateStore({ rovers: { [rover.name]: { ...rover, photo } } }))
 }
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
+
+const curiosity_rover = require('./curiosity.json');
+const curiosity_img = require('./curiosity_2023-09-08.json');
 
 const app = express()
 const port = 3000
@@ -24,5 +28,45 @@ app.get('/apod', async (req, res) => {
         console.log('error:', err);
     }
 })
+
+app.get('/rovers', async (req, res) => {
+    const roverName = req.query.rover_name
+    if (!roverName) {
+        const error = new Error('Missing Parameters');
+        error.status = 400
+        res.status(error.status).json({ error: error.message });
+    }
+
+    // res.send(curiosity_rover);
+
+    try {
+        let rover = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send(rover)
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+app.get('/roverimage', async (req, res) => {
+    const roverName = req.query.rover_name
+    const earthDate = req.query.earth_date
+    if (!roverName || !earthDate) {
+        const error = new Error('Missing Parameters');
+        error.status = 400
+        res.status(error.status).json({ error: error.message });
+    }
+    // res.send({ photo: curiosity_img.photo.img_src });
+
+    try {
+        let { photos } = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?earth_date=${earthDate}&api_key=${process.env.API_KEY}&page=1`)
+            .then(res => res.json())
+        res.send({ photo: photos[0].img_src })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
